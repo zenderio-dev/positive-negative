884f604e8b07cdbc55af526d4c6ef813cd438c11
Макс <149411166+zenderio-dev@users.noreply.github.com>
2024-09-04 02:19:24 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -30,11 +30,5 @@ async def read_root():
     return {"message": "Welcome to the classification API"}
 
 
-if __name__ == '__main__':
-    import uvicorn
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host='0.0.0.0', port=port)
-
-
 # CHANGE THIS to something you want your machine learning model to classify
 
